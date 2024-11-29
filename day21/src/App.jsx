1088cd54aa25d2a6ff1bdcd3a26955bef0083c0b
Vineet <infinite2202@gmail.com>
2024-11-29 20:31:17 +0530

    day 21

---
+++ day21/src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from "./components/Welcome"
+const App = () => {
+  return (
+    <div>
+      <h1>VINEET PRAJAPATI</h1>
+      <Welcome />
+      <p>This is my first React app.</p>
+    </div>
+  )
+}
+
+export default App
