de9ecafd634a86e150dfde7e96e3cc6341bc0615
Vineet <infinite2202@gmail.com>
2024-12-14 09:26:43 +0530

    project set-up for blog app

---
+++ day36-40/src/App.jsx
@@ -0,0 +1,12 @@
+// Dec 15-19, 2024
+// Day 36-40: Build a React project (e.g., a blog or e-commerce frontend) that uses state, context, and routing.
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-gray-700">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
