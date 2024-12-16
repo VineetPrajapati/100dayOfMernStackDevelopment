ddeb5b441850a4135d256e6c74d13827e8c74e5d
Vineet <infinite2202@gmail.com>
2024-12-16 21:44:28 +0530

    BlogCard

--- day36-40/src/components/BlogCard.jsx
+++ day36-40/src/components/BlogCard.jsx
@@ -5,24 +5,51 @@ import { Link } from "react-router-dom";
 const BlogCard = () => {
   const { posts, loading, error } = useContext(BlogContext);
 
-  if (loading) return <h2 className="text-center mt]">Loading post...</h2>;
+  if (loading) return <h2 className="text-center mt-6]">Loading post...</h2>;
   if (error) return <h2 className="text-center mt-6 text-red-500">{error}</h2>;
   return (
-    <div className="m-4 grid grid-cols-1 md:grid-cols-2 gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="bg-gray-100 p-4 rounded shadow">
-            <h3 className="text-xl font-semibold">{post.title}</h3>
-            <p className="text-gray-700">{post.body}...</p>
-            <Link
-              to={`/post/${post.id}`}
-              className="text-blue-600 hover:underline"
-            >
-              Read More
-            </Link>
-          </div>
-        ))}
-      </div>
-  )
-}
+    <div className="container mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {posts.map((post) => {
+        const randomImages = `https://picsum.photos/300/200?random=${post.id}`;
+        const bgColorClasses = [
+          "bg-blue-100",
+          "bg-green-100",
+          "bg-yellow-100",
+          "bg-red-100",
+        ];
+        const bgColor = bgColorClasses[post.id % bgColorClasses.length];
 
-export default BlogCard
+        return (
+          <div
+            key={post.id}
+            className={`rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transit duration-300 ${bgColor}`}
+          >
+            {/* Random Images */}
+            <img
+              src={randomImages}
+              alt="Blog Post"
+              className="w-full h-40 sm:h-48 md:h-56 object-cover"
+            />
+            {/* blog */}
+            <div className="p-4 flex flex-col">
+              <h3 className="text-xl md:text-xl font-semibold mb-2 text-gray-800">
+                {post.title}
+              </h3>
+              <p className="text-sm md:text-base text-gray-600 mb-4">
+                {post.body.slice(0, 100)}...
+              </p>
+              <Link
+                to={`/post/${post.id}`}
+                className="text-blue-600 hover:underline font-medium self-start"
+              >
+                Read More
+              </Link>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BlogCard;
