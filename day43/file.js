5c9a9b54b70b2f906cd710144f8d013fd6d9bc43
Vineet <infinite2202@gmail.com>
2024-12-21 04:04:23 +0530

    day43

---
+++ day43/file.js
@@ -0,0 +1,22 @@
+// Dec 22, 2024
+// Day 43: Work with file system and path modules.
+
+const fs = require("fs");
+
+// fs.writeFileSync("text.txt", "hello how are you!");
+
+// const file = fs.readFileSync("./text.txt", "utf-8");
+// console.log(file);
+
+// fs.readFile("./text.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.appendFileSync("./text.txt", `\nhello ji ${Date.now()}`);
+
+// fs.writeFileSync("./delFile.txt", "you will be deleted!");
+// fs.unlinkSync("./delFile.txt");
+
+// fs.mkdirSync("./hello");
+
---
+++ day43/path.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+const filePath ="C:/Users/hp/Desktop/100dayOfMernStackDevelopment/day43/text.txt";
+
+// const baseName =  path.basename(filePath);
+// console.log(baseName);
+
+// const dirName = path.dirname(filePath);
+// console.log(dirName);
+
+// const joinKeyword =  path.join(__dirname, "example.txt");
+// console.log(joinKeyword);
+
+const fileName =  path.join(__filename, "exp1.js");
+console.log(fileName);
+
+
+
+
+
