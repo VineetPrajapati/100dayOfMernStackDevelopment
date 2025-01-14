2ded45e25ce95c226373547ee02a1dacbe4e38e2
Vineet <infinite2202@gmail.com>
2025-01-14 05:52:34 +0530

    postSchema

---
+++ day67/models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  user: String,
+  text: String,
+});
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: String,
+  comments: [commentSchema],
+});
+
+const Post = mongoose.model("Post", postSchema);
