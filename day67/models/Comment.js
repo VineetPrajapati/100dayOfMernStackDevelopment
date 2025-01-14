c103f203d9fdb8946c93011ff6df9576689318e4
Vineet <infinite2202@gmail.com>
2025-01-14 05:57:35 +0530

    commentSchema

---
+++ day67/models/Comment.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  user: String,
+  text: String,
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
