c79a13d0e3cc5b7f1e3708b8621479f5370cb6bc
Vineet <infinite2202@gmail.com>
2025-01-15 19:42:04 +0530

    all routes done

--- day69-70/blog-api/routes/postRoutes.js
+++ day69-70/blog-api/routes/postRoutes.js
@@ -4,6 +4,7 @@ const postController = require("../controllers/postController");
 
 router.get("/", postController.getAllPosts);
 router.get("/posts/:id", postController.getPostById);
+router.get("/add-post", postController.addPost);
 router.post("/posts", postController.createPost);
 router.post("/posts/:id", postController.updatePost);
 router.post("/posts/:id/delete", postController.deletePost);
