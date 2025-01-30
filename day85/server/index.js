15aac62ac256b0c2fa8a84a45b6f32620ee1d4cb
Vineet <infinite2202@gmail.com>
2025-01-30 23:11:31 +0530

    server side endpoint done👍

---
+++ day85/server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// mongodb
+connectDB();
+
+// routes
+app.use("/api/auth", authRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
