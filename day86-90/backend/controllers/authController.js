98489ff6a0c9dd9b75087f35815cc94d4f8fcce1
Vineet <infinite2202@gmail.com>
2025-02-01 19:33:30 +0530

    userAuth

---
+++ day86-90/backend/controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully", user });
+  } catch (err) {
+    res.status(400).json({ message: "Registration failed", err });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: "Invalid credential" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res
+      .cookie("token", token, { httpOnly: true })
+      .json({ message: "Login successful", user, token });
+  } catch (err) {
+    res.status(400).json({ message: "Login failed", err });
+  }
+};
