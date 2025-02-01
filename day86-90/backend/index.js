2dc053956b903f953c766c8f8c182d6034a14543
Vineet <infinite2202@gmail.com>
2025-02-01 19:34:46 +0530

    index.js express server

---
+++ day86-90/backend/index.js
@@ -0,0 +1,30 @@
+// Feb 02 - 06, 2025
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// database
+connectDB();
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
