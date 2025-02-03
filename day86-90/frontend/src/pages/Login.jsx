f2976383bc6c7f96b7c406547f138ef5e882f458
Vineet <infinite2202@gmail.com>
2025-02-03 19:10:24 +0530

    login page

---
+++ day86-90/frontend/src/pages/Login.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../redux/authSlice";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8080/api/auth/login", {
+        email,
+        password,
+      });
+      // alert(res.data);
+      dispatch(loginSuccess(res.data));
+      // alert("Login Successful");
+      navigate("/dashboard")
+    } catch (err) {
+      console.log("Login Failed:", err);
+      alert("Login failed, try again!");
+    }
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <form
+        onSubmit={handleLogin}
+        className="bg-white p-6 rounded shadow-md w-96"
+      >
+        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="w-full p-2 border border-gray-300 rounded mb-3"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="w-full p-2 border border-gray-300 rounded mb-3"
+        />
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-700 cursor-pointer"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
