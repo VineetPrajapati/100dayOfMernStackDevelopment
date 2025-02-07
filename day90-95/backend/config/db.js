3c702e65512117503e0102f2564aa64f581b68f0
Vineet <infinite2202@gmail.com>
2025-02-07 22:04:17 +0530

    config

---
+++ day90-95/backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from mongoose;
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Yaaahooo🎉, MongoDB Connected Successfully!")
+        
+    } catch (err) {
+        console.err("Database connection error", err);
+        
+    }
+}
