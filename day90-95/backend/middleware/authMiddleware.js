9a21422a079a5e0c26b00bd6d847977eb490c805
Vineet <infinite2202@gmail.com>
2025-02-08 11:52:17 +0530

    authMiddleware

---
+++ day90-95/backend/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+// middleware to verify token
+export const protect = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(403).json({ message: "No token provided" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+// middleware for role-based access
+export const restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+    next();
+  };
+};
